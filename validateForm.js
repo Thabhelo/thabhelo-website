076e7098bf1b0aa26c510fe87f8fa09b78de6e1b
Thabhelo <thabheloduve@gmail.com>
2023-09-11 05:56:58 -0500

    Descriptive commit message here

--- validateForm.js
+++ validateForm.js
@@ -1,47 +1,60 @@
 function validateForm() {
-    var name = document.forms["myForm"]["name"].value;
-    var surname = document.forms["myForm"]["surname"].value;
-    var email = document.forms["myForm"]["email"].value;
-    var phone = document.forms["myForm"]["phone"].value;
-  
-    if (name == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  
-    if (surname == "") {
-      alert("Surname must be filled out");
-      return false;
-    }
-  
-    if (email == "") {
-      alert("Email must be filled out");
-      return false;
-    } else {
-      var emailRegex = /\S+@\S+\.\S+/;
-      if (!emailRegex.test(email)) {
-        alert("Invalid email format");
-        return false;
-      }
-    }
-  
-    if (phone != "") {
-      var phoneRegex = /^\d{10}$/;
-      if (!phoneRegex.test(phone)) {
-        alert("Invalid phone number");
-        return false;
-      }
-    }
-  
-    return true;
+  // Get form inputs by their IDs
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var phone = document.getElementById("phone").value;
+  var toggle = document.getElementById("acknowledgements-toggle");
+  var content = document.getElementById("acknowledgements-content");
+
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
   }
 
-  var toggle = document.getElementById("acknowledgements-toggle");
-var content = document.getElementById("acknowledgements-content");
+  if (surname == "") {
+    alert("Surname must be filled out");
+    return false;
+  }
+
+  // Check if name and surname are not empty
+  if (name.trim() === "" || surname.trim() === "") {
+    alert("Name and surname are required fields.");
+    return false; // Prevent form submission
+  }
+
+  if (email == "") {
+    alert("Email must be filled out");
+    return false;
+  } else {
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // Regular expressions for email validation
+    if (!emailRegex.test(email)) {
+      alert("Invalid email format");
+      return false;
+    }
+  }
+
+  if (phone != "") {
+    var phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phone)) {
+      alert("Invalid phone number");
+      return false;
+    }
+  }
+
+  // Check if the message is not empty
+  if (message.trim() === "") {
+    alert("Message field cannot be empty.");
+    return false; // Prevent form submission
+  }
+
+  return true; // If all validations pass, the form will be submitted
+}
 
 content.style.display = "none"; // hide acknowledgements content by default
 
-toggle.addEventListener("click", function(e) {
+toggle.addEventListener("click", function (e) {
   e.preventDefault();
   if (content.style.display === "none") {
     content.style.display = "block";
@@ -49,4 +62,3 @@ toggle.addEventListener("click", function(e) {
     content.style.display = "none";
   }
 });
-  
